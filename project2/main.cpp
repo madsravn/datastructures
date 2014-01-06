c6d31d22328b2ae6e99faafa0975eb4cfcaa3dac
simjesp <simjesp@gmail.com>
2014-01-06 16:25:12 +0100

    fixed main

--- project2/main.cpp
+++ project2/main.cpp
@@ -9,26 +9,25 @@
 #include "VEBTester.h"
 
 int main() {
-	/*
 	std::cout << "Queue Testing" << std::endl;
 	std::cout << "----- vEB -----" << std::endl;
 	auto tester = QueueTester(std::make_shared<VEBTree>());
-	tester.run(5);
+	tester.run(10);
 
 	std::cout << "----- BHeap -----" << std::endl;
 	tester = QueueTester(std::make_shared<BHeap>());
-	tester.run(5);
+	tester.run(10);
 
 	std::cout << "----- FHeap -----" << std::endl;
 	tester = QueueTester(std::make_shared<FHeap>());
-	tester.run(5);
-	*/
+	tester.run(10);
+	
 	std::cout << "Search Tree Testing" << std::endl;
 	std::cout << "----- vEB -----" << std::endl;
-	VEBTester().run(5);
+	VEBTester().run(10);
 
 	std::cout << "----- RB -----" << std::endl;
-	RBTester().run(5);
+	RBTester().run(10);
 
-	std::cout << "Press any key" << std::endl;
+	system("pause");
 }
